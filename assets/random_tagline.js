0dedd058c7cf1dbe9b9f9ad83a733f55fde060e1
matheger <34485360+matheger@users.noreply.github.com>
2021-08-01 12:01:46 -0600

    Update random_tagline.js

--- assets/random_tagline.js
+++ assets/random_tagline.js
@@ -3,6 +3,7 @@ var taglines = [
 	"\"Just remember that a CPU is literally a rock that we tricked into thinking.\"",
 	"<a href='https://www.youtube.com/watch?v=yneJIxOdMX4'>You look like a thing and I love you.</a>",
 	"Statistically speaking, Vatican City is home to two popes per square kilometer.",
+	"e = π = 3, for sufficiently large e and small π."
 	]
 
 function getTagline() {
